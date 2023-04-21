12de0855f49e7afc562ad19608f322a426d12ee9
psk <psk3060@naver.com>
2023-04-22 01:10:57 +0900

    용도 수정 - 원래용도 : ResponseData Wrap - 수정(형변환 예외) : ResponseEntity가 아닐 경우 생성

--- src/main/java/com/example/restservice/advice/ResponseDataAdvice.java
+++ src/main/java/com/example/restservice/advice/ResponseDataAdvice.java
@@ -1,46 +1,60 @@
 package com.example.restservice.advice;
 
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.restservice.core.ResponseDataVo;
 
-@RestControllerAdvice
+import jakarta.servlet.http.HttpServletResponse;
+
+@Aspect
+@Component
 public class ResponseDataAdvice {
 	
 	private final Logger logger = LoggerFactory.getLogger(ResponseDataAdvice.class);
 	
+	@ModelAttribute
+    public void setResponseHeader(HttpServletResponse response) {
+        response.setHeader("Content-Type", "application/json");
+        
+    }
+	
 	/**
 	 * Pointcut 내부에서 ResponseDataVo만 입력할 경우 warning no match for this type name 발생하여, 패키지명까지 함께 입력
 	 */
-	@Pointcut("within(com.example.restservice.controller..*) && execution(com.example.restservice.core.ResponseDataVo *(..))")
+	@Pointcut("within(com.example.restservice.controller..*)")
 	public void restControllerPointCut() { }
 	
-	@ResponseBody
 	@Around("restControllerPointCut()")
-	public ResponseEntity<ResponseDataVo> wrapResponseData(ProceedingJoinPoint joinPoint) throws Throwable {
+	public Object wrapResponseData(ProceedingJoinPoint joinPoint) throws Throwable {
 		
-		logger.error(joinPoint.toShortString());
+		Object returnValue = joinPoint.proceed();
 		
-		Object result = joinPoint.proceed();
-		
-		if( !(result instanceof ResponseDataVo) ) {
-			throw new Exception("NOT ResponseDataVo IS result");
+		// ResponseEntity가 아닐 경우
+		if( !(returnValue instanceof ResponseEntity) ) {
+			
+			if( !(returnValue instanceof ResponseDataVo) ) {
+				throw new IllegalArgumentException("올바르지 않은 Response");
+				
+			}
+			
+			ResponseDataVo data = (ResponseDataVo) returnValue;
+			
+			return ResponseEntity.ok(data);
 			
 		}
 		
-		ResponseDataVo responseData = (ResponseDataVo)result;
+		logger.info("returnValue value is {}", returnValue.toString());
 		
-		logger.error(responseData.toString());
+		return returnValue;
 		
-		return new ResponseEntity<ResponseDataVo>(responseData, HttpStatusCode.valueOf(200));
-
 	}
 }
